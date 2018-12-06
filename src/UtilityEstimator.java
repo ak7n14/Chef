d23f037a06a810005ec0bdd033bfbfd788678b8b
Anish Katariya <ak7n14@soton.ac.uk>
2018-12-06 23:29:32 +0000

    Estimator complete

--- src/UtilityEstimator.java
+++ src/UtilityEstimator.java
@@ -26,27 +26,45 @@ public class UtilityEstimator extends AdditiveUtilitySpaceFactory {
         for(Bid bid : bidsRankingList.getBidOrder()) {
 
             p+=1;
-            List<Issue> isssuList = bid.getIssues();
-            for(Issue issue:isssuList){
+            List<Issue> issueList = bid.getIssues();
+            for(Issue issue:issueList){
                 int issueNumber = issue.getNumber();
                 ValueDiscrete val = (ValueDiscrete) bid.getValue(issueNumber);
                 HashMap<ValueDiscrete, List<Integer>> valuePos = valsOfIssues.computeIfAbsent(issue, k -> new HashMap<>());
                 List<Integer> positionList = valuePos.computeIfAbsent(val, k -> new ArrayList<>());
                 positionList.add(p);
             }
+
+            HashMap<Issue, HashMap<ValueDiscrete, Double>> means = new HashMap<>();
+            HashMap<Issue, List<Double>> stddeviations = new HashMap<>();
+            for(Issue issue: valsOfIssues.keySet()){
+                HashMap<ValueDiscrete, List<Integer>> valuePos = valsOfIssues.get(issue);
+                ArrayList<Double> stddevlist = new ArrayList<>();
+                for(ValueDiscrete value : valuePos.keySet()){
+                    List<Integer> positions  = valuePos.get(value);
+                    double mean = positions.stream().mapToInt(e -> e).average().orElseThrow(() -> new RuntimeException("Value exists with no positions for it"));
+                    double stddev = 0d;
+                    for(int v : positions){
+                        stddev += (v - mean) * (v - mean);
+                    }
+                    stddev /= (double) positions.size();
+
+
+                    means.computeIfAbsent(issue, k -> new HashMap<>()).put(value, mean);
+
+                    stddevlist.add(stddev);
+                    this.setUtility(issue, value, mean);
+
+                }
+                Double maxStddev = stddevlist.stream().mapToDouble(e -> e).max().getAsDouble();
+                getUtilitySpace().setWeight(issue, 1d / maxStddev);
+            }
+            this.normalizeWeightsByMaxValues();
+
         }
 
     }
 
-
-    public void estimateUtility(BidRanking bids){
-
-        populateValsIssues(bids);
-        HashMap<Issue, HashMap<ValueDiscrete, Double>> means = new HashMap<>();
-        HashMap<Issue, List<Double>> stddevs = new HashMap<>();
-
-
-    }
 
 
 
